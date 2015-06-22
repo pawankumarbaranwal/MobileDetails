fc60fe49f3b49d8ddce3673a9120c7a2ace08d1e
pawankumarbaranwal <pawankumarbaranwal@gmail.com>
2015-06-23 01:40:47 +0530

    Final Commit

--- app/src/main/java/com/example/pawan/mobiledetails/MainActivity.java
+++ app/src/main/java/com/example/pawan/mobiledetails/MainActivity.java
@@ -1,18 +1,81 @@
 package com.example.pawan.mobiledetails;
-
-import android.support.v7.app.ActionBarActivity;
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.app.Activity;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+public class MainActivity extends Activity {
 
-public class MainActivity extends ActionBarActivity {
+    TextView mac,imei,email;
+    final Context context=this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mac=(TextView)findViewById(R.id.mac);
+        imei=(TextView)findViewById(R.id.imei);
+        email=(TextView)findViewById(R.id.email);
+
+        mac.setText("IMEI  "+getIMEI());
+        imei.setText("MAC  "+getMacAddress());
+        email.setText("Email  "+getEmail(context));
     }
+
+    private String getIMEI() {
+
+
+        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        String imei=telephonyManager.getDeviceId();
+        return imei;
+    }
+
+    private String getMacAddress() {
+        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wInfo = wifiManager.getConnectionInfo();
+        String macAddress = wInfo.getMacAddress();
+        return macAddress;
+    }
+
+
+    static String getEmail(Context context) {
+        AccountManager accountManager = AccountManager.get(context);
+        Account account = getAccount(accountManager);
+
+        if (account == null) {
+            return null;
+        } else {
+            return account.name;
+        }
+    }
+
+    private static Account getAccount(AccountManager accountManager) {
+        Account[] accounts = accountManager.getAccountsByType("com.google");
+        Account account;
+        if (accounts.length > 0) {
+            account = accounts[0];
+        } else {
+            account = null;
+        }
+        return account;
+    }
+
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -27,12 +90,9 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
